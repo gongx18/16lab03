1512b80f46bbed203cbb375fe89672fcd0195244
Xi Gong <xi@ucsb.edu>
2018-11-13 07:59:08 -0800

    Version 3.0

--- myProg01.cpp
+++ myProg01.cpp
@@ -1,32 +1,17 @@
-#include <cstdlib>  // for exit(), perror()
- #include <fstream>  // for ifstream
- #include <iostream> // for printf()
- #include <string>
+// myProg01.cpp
+//"Author: Xi Gong"
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // Simple that counts down from n to 1"
+ 
+    int n=5;
+ 
+    for (int i=n; i>0; i--) {
+       cout << i << " ";
+    }
+    cout << "/n";
+    return 0;
+}
 
- using namespace std;
-
- int main(int argc, char *argv[]) {
-   if (argc != 2) {
-     // if argc is not 2, print an error message and exit
-     cerr << "Usage: " << argv[0] << " inputFile" << endl;
-     exit(1); // defined in cstdlib
-   }
-
-   ifstream in(argv[1]);
-
-  // if (!in) {
-  //   cerr << "error opening file" << endl;
-  //   exit(1);
-  // }
-
-   string str;
-   int count = 0;
-   while (in >> str) {
-     // in >> str : can be a boolean to check wheather there's input.
-     if (str == "duck")
-       count++;
-   }
-
-   cout << "There were " << count << " ducks in " << argv[1] << endl;
- }
-                                                                                                                                                              
